72042a52dbf834a6020f021532e2983fbe960697
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-11-28 12:45:00 -0800

    Update longest_palindrome.js

--- longest_palindrome.js
+++ longest_palindrome.js
@@ -15,3 +15,13 @@ Example:
 
 
 // Solution
+
+let longestPalindrome = function(s) {
+  if (!s) return 0;
+  for (let c = s.length; c > 0; c--) {
+    for (let i = 0; i <= s.length - c; i++) {
+      let check = s.substr(i, c);
+      if (check === check.split("").reverse().join("")) return c;
+    }
+  }
+}
