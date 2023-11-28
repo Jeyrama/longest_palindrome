b9a1697eea1aa6fc06be87c33196c112d46dc7d3
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-11-28 12:57:52 -0800

    Update longest_palindrome.js

--- longest_palindrome.js
+++ longest_palindrome.js
@@ -27,3 +27,24 @@ let longestPalindrome = function(s) {
 }
 
 // or
+
+longestPalindrome = function(s) {
+  let longest = 0;
+  let length = s.length;
+
+  for (let i = 0; i < length; i++) {
+    for (let j = i+1; j <= length; j++) {
+      let str = s.slice(i,j);
+      let l = str.length
+      if (isPalindrome(str) && longest < l) {
+        longest = l;
+      }
+    }
+  }
+  return longest;
+}
+
+function isPalindrome(s) {
+  let arr = s.split("");
+  return s == arr.reverse().join("");
+}
